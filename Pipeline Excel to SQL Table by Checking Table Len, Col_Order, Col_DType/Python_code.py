5183f3b1b329c79fd06a8f66dd42aad4207bf281
Vishwajeet Kamble <92873036+vishwajeet-kamble@users.noreply.github.com>
2023-03-15 17:41:10 +0530

    Update Python_code.py

--- Pipeline Excel to SQL Table by Checking Table Len, Col_Order, Col_DType/Python_code.py
+++ Pipeline Excel to SQL Table by Checking Table Len, Col_Order, Col_DType/Python_code.py
@@ -1,4 +1,4 @@
-# # Installing Required Libraries If not installed -- Press ( CTRl + ? ) to Uncomment -
+# # Installing Required Libraries -- Press ( CTRl + ? ) to Uncomment -
 
 # !pip3 install pymysql --quiet
 # !pip3 install ipython-sql --quiet
@@ -16,30 +16,34 @@ import warnings
 warnings.simplefilter("ignore", UserWarning)
 
 
-
 # Excel Table:
 print("Enter_Excel_path -")
 path = input()
 
 print("")
 
+# Excel workbook path --
+#path = "test_excel.xlsx"
+
 # Open the workbook and define the worksheet
 workbook = openpyxl.load_workbook(path)
 worksheet = workbook.active
 
-# Establish a MySQL connection
 # Table of MySQL database
-print("Enter SQL DBName & TableName -")
 
-print("\nSQL DB Name")
-SQL_DB = input()
+print("Enter_SQL_Database or Schema_Name -")
+schema = input()
 
-print("\nSQL Table Name")
-SQL_Table = input()
+print("")
+
+print("Enter_SQL_Table_Name -")
+table = input()
+
+print("")
 
 
-database = MySQLdb.connect (host="localhost", user = "root", passwd = "root", db = SQL_DB)
-print("\nSQL connection success")
+# Establish a MySQL connection
+database = MySQLdb.connect (host="localhost", user = "root", passwd = "root", db = schema )
 
 # Get the cursor, which is used to traverse the database, line by line
 cursor = database.cursor()
@@ -69,13 +73,11 @@ def excel_tbl_col_dt_len(rpath):
 
 excel_tbl, excel_tbl_coln_length, excel_tbl_coln, excel_tbl_coln_dtypes = excel_tbl_col_dt_len(path)
 
-
 print("")
 
 # table_name = "orders_int"
 
-sql_tbl = pd.read_sql_query("select * from " + SQL_Table, database) 
-print("Total rows in SQL Table Before adding Rows - ", sql_tbl.shape[0])
+sql_tbl = pd.read_sql_query("select * from " + table, database) 
 
 # print(sql_tbl.dtypes)
 
@@ -91,18 +93,87 @@ sql_column_len = sql_column.shape[0]
 sql_column_dtypes = sql_tbl.dtypes
 #print(sql_column_dtypes)
 
+print("SQL connection success")
+
+print("")
+
+
+print("SQL Table Column Length is - ", sql_column.shape[0] , "\nSo type '%s' with comma seperated", sql_column.shape[0], " times")
+print("\nFor example - if Length is 4 then Type like -\n", "%s, %s, %s, %s\n")
+value_len = input()
+
+print("")
+
+print("Total rows in SQL Table Before adding Rows - ", sql_tbl.shape[0])
+
+print("")
+
+sql_col_dtype = pd.read_sql_query("select column_name, DATA_TYPE from information_schema.columns where table_schema = '" + schema + "' and table_name = '" + table + "';", database)
+# SQL table column data type in sorted order
+sql_column_dtype = sql_col_dtype['DATA_TYPE']
+sql_column_sort = sql_col_dtype['COLUMN_NAME']
+
+for i in range(0, sql_column_len):
+    if sql_column_dtype[i] == "decimal":
+        
+        sql_column_dtype = [x.replace('decimal', 'float64') for x in sql_column_dtype]
+        
+    elif sql_column_dtype[i] == "datetime":
+        
+        sql_column_dtype = [x.replace('datetime', 'datetime64[ns]') for x in sql_column_dtype]
+        
+    elif sql_column_dtype[i] == "text":
+        
+        sql_column_dtype = [x.replace('text', 'object') for x in sql_column_dtype]
+        
+    elif sql_column_dtype[i] == "int":
+        
+        sql_column_dtype = [x.replace('int', 'int64') for x in sql_column_dtype]
+    
+    i = i + 1
+        
+   
+# print("sql dtypes", sql_column_dtype)
+
 #Insert Query
-query = """INSERT INTO """ + SQL_Table + """ (Date, SM, Product, SP, CP) VALUES (%s, %s, %s, %s, %s)"""
+
+query = """INSERT INTO """ + table + """ VALUES (""" +  value_len + """)"""
 
 # "INSERT INTO orders_int (Date, SM, Product, SP, CP) VALUES (%s, %s, %s, %s, %s)"
 
-print("-" * 65,"\n")
+print("-" * 65)
+
+
+# Creating DataFrame for Matching SQL & Excel Columns
+Col_Match_df = pd.DataFrame({"SQL" :sql_column,
+                            "Excel" : excel_tbl_coln}, 
+                            columns=['SQL', 'Excel'])
+
+# print(Col_Match_df)
+# print(Col_Match_df.shape)
+
+# Column_match_SQL_Excel['Match Status'] Process -
+M_S = []
+match = 'Matched'
+N_match = 'Not-Matched'
+
+for i in range(0, Col_Match_df.shape[0]):
+    if Col_Match_df['SQL'][i] == Col_Match_df['Excel'][i]:
+        M_S.append(match)
+        
+    else:
+        M_S.append(N_match)
+
+# print(M_S)   
+Col_Match_df['Match_Status'] = M_S
+
+# print(Column_match_SQL_Excel)
 
 
 # Checking Column_length, Column_order, Dtype matching for Excel and SQl if Matching then Dumping Data - 
 if excel_tbl_coln_length == sql_column_len:
     
-    print("Table Columns Length Matched\n")
+    print("Table Columns Length Matched")
 
     for i in range(0,excel_tbl_coln_length):
 
@@ -111,7 +182,7 @@ if excel_tbl_coln_length == sql_column_len:
 
             # print('Column_Name by Order Matched', i , "[", excel_tbl_coln[i], " Excel_table ", "=", sql_column[i], " SQL_table ", "]")
             
-            if excel_tbl_coln_dtypes[i] == sql_column_dtypes[i]:
+            if excel_tbl_coln_dtypes[i] == sql_column_dtype[i]:
                 
                 # print('Column_Datatype by Order Matched', i , "[", excel_tbl_coln_dtypes[i], " Excel_table ", "=", excel_tbl_coln_dtypes[i], " SQL_table ", "]")
             
@@ -130,7 +201,7 @@ if excel_tbl_coln_length == sql_column_len:
 
                 print("-" * 65)
 
-                sql_tbl_new = pd.read_sql_query("select * from " + SQL_Table, database) 
+                sql_tbl_new = pd.read_sql_query("select * from " + table, database) 
                 #print("SQL Tabel", sql_tbl_new)
                 print("Total rows in SQL Table After Adding Rows - ", sql_tbl_new.shape[0])
                 # Commit the changes and close the database connection
@@ -138,19 +209,22 @@ if excel_tbl_coln_length == sql_column_len:
                 database.close()
                 
                 print("")
-                print("Rows successfully appended to MySQL table\n")
+                print("Rows successfully appended to MySQL table.")
                 print("All Conditions Matched - Table Column Length, Column Order, Column DataType")
+                
                 
             else:
                     
-                print('Column_DataType by Order Not Matched', "Index-" , i ,  "[ Excel Column Name - ", excel_tbl_coln[i], "DataType - ", excel_tbl_coln_dtypes[i], " Excel_table ", " <> ",  " SQL Column Name - ", sql_column[i], "DataType - ", sql_column_dtypes[i],"]")
+                print('Column_DataType by Order Not Matched', "Index-" , i ,  "[ Excel Column Name - ", excel_tbl_coln[i], "DataType - ", excel_tbl_coln_dtypes[i], " Excel_table ", " <> ",  " SQL Column Name - ", sql_column_sort[i], "DataType - ", sql_column_dtype[i],"]")
 
                 break 
 
 
             
         else:
-            print('Column_Name by Order Not Matched',  "Index-" , i , "[ Excel_table Column Name - ", excel_tbl_coln[i], " <> ", " SQL_Table Column Name - " , sql_column[i], "]")
+            print('Error - Column_Name by Order Not Matched',  "Index-" , i , "[ Excel_table Column Name - ", excel_tbl_coln[i], " <> ", " SQL_Table Column Name - " , sql_column[i], "]")
+            print("")
+            print(Col_Match_df)
 
             break 
 
